57b8c4e312e354f61e0bde2634186097efbfc2da
Hakan Ensari <hakan.ensari@papercavalier.com>
2012-05-09 10:15:42 +0200

    Refactor spec

--- spec/aws/middleware/sign_request_spec.rb
+++ spec/aws/middleware/sign_request_spec.rb
@@ -3,11 +3,10 @@ require 'spec_helper'
 module AWS
   module Middleware
     describe SignRequest do
-      subject { described_class.new lambda { |env| env }, 'secret' }
-
       describe '#call' do
         it 'signs the request' do
-          res = subject.call :url => URI('http://example.com/?q=1')
+          middleware = described_class.new lambda { |env| env }, 'secret'
+          res = middleware.call :url => URI('http://example.com/?q=1')
           res[:url].query.should match /&Signature=.+/
         end
       end
