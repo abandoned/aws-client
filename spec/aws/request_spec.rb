5a46ec57ca6db40a5262a26ca365641241483919
Hakan Ensari <hakan.ensari@papercavalier.com>
2012-05-09 11:30:47 +0200

    Test too contrived

--- spec/aws/request_spec.rb
+++ spec/aws/request_spec.rb
@@ -3,17 +3,5 @@ require 'spec_helper'
 module AWS
   describe Request do
     it_behaves_like 'a request'
-
-    context 'when subclassed' do
-      describe '#endpoint' do
-        it 'returns the subclassed endpoint' do
-          ::Foo = Module.new
-          Foo.const_set :Endpoint, Class.new(AWS::Endpoint)
-          Foo.const_set :Request, Class.new(AWS::Request)
-          req = Foo::Request.new 'US'
-          req.endpoint.class.should eq Foo::Endpoint
-        end
-      end
-    end
   end
 end
